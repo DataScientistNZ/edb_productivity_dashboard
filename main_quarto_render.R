b721bcbfb20514e621e0030238eefeacdeb799fc
raoulp <raoul.pillard@comcom.govt.nz>
2024-11-19 15:54:49 +1300

    add file to manage quarto rendering

---
+++ main_quarto_render.R
@@ -0,0 +1,22 @@
+# quarto does not allow to create output files in a subfolder
+# subfolders are required to host more content within a project on github
+# we perform a small hack to make it happen!
+# Below function creates the output in the same folder as the input
+# and plays nicely with relative directories!
+my_quarto_render <- function(input_file, ...){
+  
+  # will create tmp_quarto.html in the current directory
+  # then will move it to the input directory and name it index.html
+  # name index.html is required for github publishing process
+  
+  tmp_output_file <- file.path("tmp_quarto.html")
+  output_file <- file.path(dirname(input_file), "index.html")
+  quarto::quarto_render(input_file, output_file = tmp_output_file, ...)
+  file.rename(tmp_output_file, output_file)
+  output_file
+}
+
+
+
+my_quarto_render("quarto/quarto_dashboard.qmd")
+my_quarto_render("quarto_dashboard.qmd")
